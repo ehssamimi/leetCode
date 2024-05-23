00d6ae12f1346ebbb6d83f2fdac4f7d776a42baa
ehsan samimi <47100310+ehssamimi@users.noreply.github.com>
2024-05-23 23:15:59 +0330

    Time: 58 ms (79.53%), Space: 49.6 MB (45.87%) - LeetHub

---
+++ 2723-add-two-promises/2723-add-two-promises.js
@@ -0,0 +1,16 @@
+/**
+ * @param {Promise} promise1
+ * @param {Promise} promise2
+ * @return {Promise}
+ */
+var addTwoPromises = async function(promise1, promise2) {
+     return await Promise.all([promise1,promise2]).then((data)=>{
+        return data[0] + data[1];
+    })
+    
+};
+
+/**
+ * addTwoPromises(Promise.resolve(2), Promise.resolve(2))
+ *   .then(console.log); // 4
+ */
